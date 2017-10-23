92e5a90095d63da3509a2d38a9da7b7a897596b9
omayib <omayib@gmail.com>
2017-10-23 12:50:24 +0700

    create case 2

---
+++ Case2/app/src/main/java/id/technomotion/case2/MainActivity.java
@@ -0,0 +1,12 @@
+package id.technomotion.case2;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
